3d8086e32dc99129bba4772e966935df69755be2
rebecca martin <rbecky.powdergirl@gmail.com>
2011-07-30 13:14:56 +0100

    Becky - setting up the application framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Powder::Application.config.secret_token = '0c004256af1256ad55a207717790e34f0f886501efe6ded4bdaf12e951a7cb4b45a63cd15ed64f25f0f04744b2a80c16408f78b3cef1ef4ffd5b24920d87df93'
